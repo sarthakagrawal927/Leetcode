a4c6003dce7c54c4de7120c7f63f3080cc64120e
Sarthak Agrawal <43884471+sarthakagrawal927@users.noreply.github.com>
2022-04-21 09:36:36 +0530

    Time: 156 ms (90.79%), Space: 104.5 MB (5.32%) - LeetHub

---
+++ 896-monotonic-array/896-monotonic-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isDecreasing(vector<int> nums){
+        for(int i = 0; i < nums.size()-1; i++){
+            if(nums[i] > nums[i+1]) return false;
+        }
+        return true;
+    }
+    bool isIncreasing(vector<int> nums){
+        for(int i = 0; i < nums.size()-1; i++){
+            if(nums[i] < nums[i+1]) return false;
+        }
+        return true;
+    }
+    bool isMonotonic(vector<int>& nums) {
+        return isIncreasing(nums) || isDecreasing(nums);
+    }
+};
