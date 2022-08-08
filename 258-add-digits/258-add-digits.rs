78ed59a5dc0823a11309983a7e7e0e05359012d4
Sarthak Agrawal <43884471+sarthakagrawal927@users.noreply.github.com>
2022-08-08 21:04:04 +0530

    Time: 3 ms (21.88%), Space: 2.1 MB (28.13%) - LeetHub

--- 258-add-digits/258-add-digits.rs
+++ 258-add-digits/258-add-digits.rs
@@ -1,16 +1,5 @@
 impl Solution {
     pub fn add_digits(num: i32) -> i32 {
-        let mut temp = num;
-        while temp >= 10{
-            temp = {
-                let mut sum = 0;
-                while temp > 0 {
-                    sum += temp % 10;
-                    temp /= 10;   
-                }
-                sum
-            }
-        }
-        temp
+       if num == 0  {0} else {1 + (num -1) % 9}
     }
 }
